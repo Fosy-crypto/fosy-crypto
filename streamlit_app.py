eed02e0f1b35a1ff157699f23977effecef17b79
Fosy-crypto <fosyyy9@gmail.com>
2026-05-19 11:19:42 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,15 @@ st.write(
 )
 import streamlit as st
 
-st.text("Hello Mo")
+import streamlit as st
+import time
+
+@st.fragment
+def release_the_balloons():
+    st.button("Release the balloons", help="Fragment rerun")
+    st.balloons()
+
+with st.spinner("Inflating balloons..."):
+    time.sleep(5)
+release_the_balloons()
+st.button("Inflate more balloons", help="Full rerun")
